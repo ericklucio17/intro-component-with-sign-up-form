39245da17cde2ac9796dfb513f8ce91b2100da85
Erick Lucio <erick.adrian.lu@gmail.com>
2020-06-20 14:00:33 -0500

    Mejoras en ccs y js

--- js/main.js
+++ js/main.js
@@ -4,40 +4,63 @@ let button = document.getElementById('button');
 
 button.addEventListener('click', function () {
 
-    let firstName = document.querySelector('#first-name').value;
-    let lastName = document.querySelector('#last-name').value;
-    let emailAddress = document.querySelector('#email-address').value;
-    let password = document.querySelector('#password').value;
+    let firstName = document.querySelector('#first-name');
+    let lastName = document.querySelector('#last-name');
+    let emailAddress = document.querySelector('#email-address');
+    let password = document.querySelector('#password');
 
-    if (firstName == null || firstName.length == 0) {
+    let firstNameValue = document.querySelector('#first-name').value;
+    let lastNameValue = document.querySelector('#last-name').value;
+    let emailAddressValue = document.querySelector('#email-address').value;
+    let passwordValue = document.querySelector('#password').value;
 
-        let firstNameLabel = document.querySelector('#first-name-label');
+    let firstNameLabel = document.querySelector('#first-name-label');
+    let lastNameLabel = document.querySelector('#last-name-label');
+    let emailAddressLabel = document.querySelector('#email-address-label');
+    let passwordLabel = document.querySelector('#password-label');
 
-        document.getElementById("first-name").focus();
+    if (firstNameValue == null || firstNameValue.length == 0) {
+
+        firstNameLabel.style.display = 'inline';
         firstNameLabel.textContent = 'Firt Name cannot be empty';
+        firstName.style.outlineColor = "#FF7A7A";
+        firstName.focus();
     }
     
-    if (lastName == null || lastName.length == 0) {
+    if (lastNameValue == null || lastNameValue.length == 0) {
 
-        let lastNameLabel = document.querySelector('#last-name-label');
-
-        document.getElementById("last-name").focus();
+        lastNameLabel.style.display = 'inline';
         lastNameLabel.textContent = 'Last Name cannot be empty';
+        lastName.style.outlineColor = "#FF7A7A";
+        lastName.focus();
     }
 
-    if (emailAddress == null || emailAddress.length == 0) {
+    if (emailAddressValue == null || emailAddressValue.length == 0) {
 
-        let emailAddressLabel = document.querySelector('#email-address-label');
-
-        document.getElementById("email-address").focus();
+        emailAddressLabel.style.display = 'inline';
         emailAddressLabel.textContent = 'Email Address cannot be empty';
+        emailAddress.style.outlineColor = "#FF7A7A";
+        emailAddress.focus();
     }
 
-    if (password == null || password.length == 0) {
+    if (passwordValue == null || passwordValue.length == 0) {
 
-        let passwordLabel = document.querySelector('#password-label');
-
-        document.getElementById("password").focus();
+        passwordLabel.style.display = 'inline';
         passwordLabel.textContent = 'Password cannot be empty';
+        password.style.outlineColor = "#FF7A7A";
+        password.focus();
+    }
+
+    if (firstNameValue != "" && lastNameValue != "" && emailAddressValue != "" && passwordValue != "") {
+
+        firstName.value = "";
+        lastName.value = "";
+        emailAddress.value = "";
+        password.value = "";
+
+        firstNameLabel.style.display = 'none';
+        lastNameLabel.style.display = 'none';
+        emailAddressLabel.style.display = 'none';
+        passwordLabel.style.display = 'none';
     }
 });
